c63de6dd93dccef417fe34c0e017ca082ca2ce7e
brandsummit <web@brandsummit.es>
2026-06-02 21:45:00 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,15 @@ import { supabase } from '@/lib/supabase'
 
 export default async function Home() {
   const { data, error } = await supabase
-    .from('clients')
-    .select('*')
+    .from('quotes')
+    .select(`
+      *,
+      clients(*)
+    `)
 
   return (
     <main>
-      <h1>Conexión OK</h1>
-
       <pre>{JSON.stringify(data, null, 2)}</pre>
-
       <pre>{JSON.stringify(error, null, 2)}</pre>
     </main>
   )
